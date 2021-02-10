74fbbe36eef7e350bf78c6ef2193c692c996e856
Rameez Khan <rameezkhan.sa@gmail.com>
2021-02-10 19:15:18 +0200

    Add more inputs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     stable.url = "github:nixos/nixpkgs/nixos-20.09";
+    darwin = {
+      url = "github:kclejeune/nix-darwin/brew-bundle";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, stable }: {
