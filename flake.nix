e7a661ae776e7c1aeb7f85676fb9abe55f459462
Rameez Khan <rameezkhan.sa@gmail.com>
2021-02-10 19:21:45 +0200

    Add flake-utils

--- flake.nix
+++ flake.nix
@@ -4,17 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     stable.url = "github:nixos/nixpkgs/nixos-20.09";
-    darwin = {
-      url = "github:kclejeune/nix-darwin/brew-bundle";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, stable }: {
-
-  };
+  outputs =
+    inputs@{ self, nixpkgs, flake-utils, ... }:
+    let
+      name = "Rameez Khan"
+    in
+      flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = let
+          pyEnv = (mach-nix.lib.${system}.mkPython {
+            requirements = ''
+              black
+              pylint
+              typer-cli
+              typer
+              colorama
+              shellingham
+              distro
+            '';
+          });
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [ nixFlakes rnix-lsp pyEnv ];
+        };
+      });
 }
