32b5bcad1186294f3c7d8e3e8f502c7a6dae0797
Rameez Khan <rameezkhan.sa@gmail.com>
2021-02-11 09:22:26 +0200

    Add home.nix

---
+++ machines/home.nix
@@ -0,0 +1,85 @@
+{ inputs, config, pkgs, ... }:
+let
+  homeDir = config.home.homeDirectory;
+  nixFlakes = "${pkgs.nixFlakes}/bin/nix";
+  sysDoNixos =
+    "[[ -d /etc/nixos ]] && cd /etc/nixos && ${nixFlakes} develop -c /etc/nixos/do.py $@";
+  sysDoDarwin =
+    "[[ -d ${homeDir}/.nixpkgs ]] && cd ${homeDir}/.nixpkgs && ${nixFlakes} develop -c ${homeDir}/.nixpkgs/do.py $@";
+  sysdo = (pkgs.writeShellScriptBin "sysdo" ''
+    ${sysDoNixos} || ${sysDoDarwin}
+  '');
+in {
+  imports = [ ../modules/core.nix ];
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "20.09";
+    sessionVariables = {
+      GPG_TTY = "/dev/ttys000";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      CLICOLOR = 1;
+      LSCOLORS = "ExFxBxDxCxegedabagacad";
+      KAGGLE_CONFIG_DIR = "${config.xdg.configHome}/kaggle";
+      JAVA_HOME = "${pkgs.jdk11}";
+    };
+
+    # define package definitions for current user environment
+    packages = with pkgs; [
+      # nix stuff
+      nixpkgs-fmt
+      nixfmt
+      niv
+      sysdo
+
+      # scripting
+      (python39.withPackages
+        (ps: with ps; [ bpython black numpy scipy networkx ]))
+
+      # gnu stuff
+      # encryption and signing utilities
+      gnupg
+      gawk
+      gnused
+      gnugrep
+
+      # dev garbage
+      nodejs
+      yarn
+      pre-commit
+      jq
+      jdk11
+      kotlin
+
+      # command line utilities
+      git
+      curl
+      wget
+      ranger
+      rsync
+      httpie
+      pandoc
+      ripgrep
+      ripgrep-all
+
+      # other useful stuff
+      youtube-dl
+      speedtest-cli
+
+      # typesetting
+      # (texlive.combine { inherit (texlive) scheme-basic latexindent latexmk; })
+      texlive.combined.scheme-full
+      # tectonic
+    ];
+  };
+}
